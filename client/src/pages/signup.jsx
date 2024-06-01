39549bad4d0ddd5897df9e5abd2f9c66fd87d036
lordstorein <lordstorein@gmail.com>
2024-06-01 02:46:00 -0700

    new

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -101,7 +101,7 @@ export const Signup = ()=>{
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
             </div>
  
-            <input id="password" type="password" name="password" value={user.password} onChange={handleInput} className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" required />
+            <input id="password" type="password" name="password" value={user.password} onChange={handleInput} className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Create Password" required />
           </div>
         </div>
 
