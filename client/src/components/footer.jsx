237cf749667db0e35768dca559f3a52fff1bf1f9
lordstorein <lordstorein@gmail.com>
2024-06-01 01:00:35 -0700

    new

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,9 +4,9 @@ export const Footer = ()=>{
         <div className='font-inter bg-[#4d61fc] h-[80px] mt-auto text-white flex justify-center items-center flex-col gap-3'>
             <div>
                 <ul className='flex justify-center items-center gap-8 sm:text-[15px] text-[13px] ' >
-                    <Link to='https://instagram.com/lordstore.shop'><li className='cursor-pointer'>Instagram</li></Link>
+                    <Link to='https://instagram.com/lordstore.shop'  target="_blank"><li className='cursor-pointer'>Instagram</li></Link>
                     <Link to=''><li className='cursor-pointer'>Youtube</li></Link>
-                    <Link to='https://instagram.com/axelod.ae'><li className='cursor-pointer'>Admin</li></Link>
+                    <Link to='https://instagram.com/axelod.ae' target="_blank"><li className='cursor-pointer'>Admin</li></Link>
                 </ul>
             </div>
             <div >
