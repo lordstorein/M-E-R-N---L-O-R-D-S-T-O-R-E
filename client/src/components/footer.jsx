9aa57020b06bf051bc4101d20beea868ef2082bd
lordstorein <lordstorein@gmail.com>
2024-06-01 00:57:12 -0700

    new

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,9 +4,8 @@ export const Footer = ()=>{
         <div className='font-inter bg-[#4d61fc] h-[80px] mt-auto text-white flex justify-center items-center flex-col gap-3'>
             <div>
                 <ul className='flex justify-center items-center gap-8 sm:text-[15px] text-[13px] ' >
-                    <Link to='/termsandconditions'><li className='cursor-pointer'>Terms&conditions</li></Link>
-                    <Link to='/privacypolicy'><li className='cursor-pointer'>Privacy Policy</li></Link>
-                    <Link to='/refundpolicy'><li className='cursor-pointer'>Refund Policy</li></Link>
+                    <Link to='https://instagram.com/lordstore.shop'><li className='cursor-pointer'>Instagram</li></Link>
+                    <Link to=''><li className='cursor-pointer'>Youtube</li></Link>
                 </ul>
             </div>
             <div >
